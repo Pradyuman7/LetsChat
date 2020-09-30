67d486769b21d06ad67322724572a3bb84b4a191
Mukul Prabhakar <me24mukul@gmail.com>
2020-10-01 00:07:24 +0530

    Update 01-Oct-2020
    1. Migrated to Android X
    2. Updated gradle to new and build target to API 16 to API 30
    3. IMPORTANT - Issue resolved :- Unable to register to app i.e., resolved crash
    4. Updated UI for Register.java and Login.java for edit box
    5. Implemented View Binding for Register and Login Activity and Optimized code for Register and Login Page.

--- app/src/main/java/com/pd/chatapp/Users.java
+++ app/src/main/java/com/pd/chatapp/Users.java
@@ -2,13 +2,14 @@ package com.pd.chatapp;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
